85fa9197f0edd800460e7e54eb6bde886f2835a9
jessica <jessica.mrey>
2019-08-29 19:24:29 +0200

    borrados archivos subidos por error

--- src/environments/environment.local.ts
+++
@@ -1,9 +0,0 @@
-
-export const environment = {
-  production: false,
-  backendUrl: 'http://localhost:8000',
-  loginSeoUrl:'http://www.seguimientodeaves.org/_Atlas/frmLoginCensosServer.php',
-  grant_type: "password",
-  client_secret: "1utbw9ki61og0008ko8ko0gskg4ckgsk08g8gowkk4w8wgkg48",
-  client_id: "1_37v2qyyqfw4kww444c888co8o084soscos0owcwwcs8g00c8ck"
-};
